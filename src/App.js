03095806c028bd27bf3326b0aabd36ce6fef5269
Furkan Demirtürk <42823006+fdmrtrk@users.noreply.github.com>
2020-02-11 21:21:52 +0300

    loading animation

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="anomation-one">
+        <div className="loader">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
